45051fec16bdd3dca101ddf72efdf21cffb4e2f7
JBANKENHOMELAP\joe <jbanken@gmail.com>
2017-08-15 21:19:55 -0500

    Mocked in a nav bar with a Home, Customers, and Schedule controller.

--- meadow-runner/src/app/app.module.ts
+++ meadow-runner/src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CustomersComponent } from './customers/customers.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'customers',      component: CustomersComponent },
+  { path: 'schedule',      component: ScheduleComponent }
+  //{ path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent
+    , HomeComponent
+    , CustomersComponent
+    , ScheduleComponent
   ],
   imports: [
     BrowserModule
+    ,RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
